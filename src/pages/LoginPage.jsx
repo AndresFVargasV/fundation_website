ce9ebc9534702d45d7841a6b92826724c8e4979a
Bryam Anderson Villalobos De Leon <bryamv@uninorte.edu.co>
2023-07-22 10:38:26 -0500

    loginForm componentized

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { useForm } from "../hooks/useForm";
+import LoginForm from "../components/LoginForm";
 
 const LoginPage = () => {
   const [{ email, password }, handleInputChange, reset] = useForm({
@@ -27,63 +27,12 @@ const LoginPage = () => {
               </div>
 
               <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                <form style={{ width: "23rem" }} onSubmit={handlesubmit}>
-                  <h3
-                    className="fw-normal mb-3 pb-3"
-                    style={{ letterSpacing: "1px" }}
-                  >
-                    Iniciar sesión
-                  </h3>
-
-                  <div className="form-outline mb-4">
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      value={email}
-                      className="form-control form-control-lg"
-                      onChange={handleInputChange}
-                    />
-                    <label className="form-label" htmlFor="email">
-                      Correo
-                    </label>
-                  </div>
-
-                  <div className="form-outline mb-4">
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      value={password}
-                      className="form-control form-control-lg"
-                      onChange={handleInputChange}
-                    />
-                    <label className="form-label" htmlFor="password">
-                      Contraseña
-                    </label>
-                  </div>
-
-                  <div className="pt-1 mb-4">
-                    <button
-                      className="btn btn-info btn-lg btn-block"
-                      type="submit"
-                    >
-                      Iniciar sesión
-                    </button>
-                  </div>
-
-                  <p className="small mb-5 pb-lg-2">
-                    <Link to="/restore" className="text-muted">
-                      ¿Olvidaste tu contraseña?
-                    </Link>
-                  </p>
-                  <p>
-                    ¿No tienes una cuenta?{" "}
-                    <Link to="/register" className="link-info">
-                      Registrate Aquí
-                    </Link>
-                  </p>
-                </form>
+                <LoginForm
+                  handlesubmit={handlesubmit}
+                  email={email}
+                  password={password}
+                  handleInputChange={handleInputChange}
+                />
               </div>
             </div>
             <div className="col-sm-6 px-0 d-none d-sm-block">
